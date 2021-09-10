be5d217502ae98ae29ecf21cc62d5020e36c0cf0
Ícaro <icaro.moreira@protonmail.com>
2021-09-10 18:52:26 -0300

    removing useless spaces

--- api-gateway/src/apiGateway.ts
+++ api-gateway/src/apiGateway.ts
@@ -15,26 +15,20 @@ APP.use(express.json())
 function selectProxyHost(request: Request){
 
     if (request.path.startsWith("/movies")){
-
-        console.log(request.path)
         return "http://localhost:3001/"
     }
 
     if (request.path.startsWith("/cinema")){
-
         return "http://localhost:3002/"
     }
 
     else {
-
         throw new Error("Rota inválida")
     }
 }
 
 APP.use((request: Request, response: Response, next: NextFunction) => {
-
     httpProxy(selectProxyHost(request))(request, response, next)
-
 });
 
 APP.use(ErrorHandler)
